2e2c0af815a3a9df157171530554a050ac7a1777
jonniebigodes <jonniebigodes@hotmail.com>
2020-06-02 22:05:47 +0100

    fixing case sense with netlify...ohh boy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import TodoList from '../components/todoList'
 const IndexPage = () => (
   <div>
     <h1 style={{ textAlign: "center" }}>Recoil being used with Gatsby</h1>
-    <TodoList />
+   zoinks
   </div>
 )
 export default IndexPage
