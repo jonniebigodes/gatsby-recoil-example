f0217dc8c03dc7ac7b1792e37da4ef075cbab7c8
jonniebigodes <jonniebigodes@hotmail.com>
2020-06-02 22:12:12 +0100

    attempt to fix build..with case sensitivity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import TodoList from '../components/TodoList'
 const IndexPage = () => (
   <div>
     <h1 style={{ textAlign: "center" }}>Recoil being used with Gatsby</h1>
-   zoinks
+   <TodoList/>
   </div>
 )
 export default IndexPage
